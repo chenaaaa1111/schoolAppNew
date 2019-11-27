d66201086f270bf83994d590f7a4b1f499defa5e
chenyang <124035247@qq.com>
2019-11-27 21:27:49 +0800

    删掉er

--- src/views/home.vue
+++ src/views/home.vue
@@ -139,10 +139,10 @@
       },
       getUserInfo(){
         var userInfo=sessionStorage.getItem('userInfo');
-        this.userInfo=JSON.parse(userInfo); 
+        this.userInfo=JSON.parse(userInfo);
         return JSON.parse(userInfo);
       },
-      formatObj(){er
+      formatObj(){
         var self=this;
         var realRoutlist=[];
         var userInfo=this.getUserInfo();
