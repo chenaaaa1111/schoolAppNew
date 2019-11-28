beb5094028fe1023be7b8ef45cb46c5c8269cd91
chendq <934471808@qq.com>
2019-11-29 00:05:47 +0800

    ss

--- src/views/classes/campusHomepage/components/mainNavBar.vue
+++ src/views/classes/campusHomepage/components/mainNavBar.vue
@@ -9,8 +9,9 @@
               </ul> -->
       <div class="tabContainer">
         <div class="leftBar">
-          <van-tabs class="mainleftbar" :swipe-threshold='5' :ellipsis="false" v-model="selectTab" @change="changeTabs" :swipeable="true">
-            <van-tab v-for="item in dataList" :title="item.title" :name="item.id" :key="item.id" >
+          <van-tabs class="mainleftbar" :swipe-threshold='5' :ellipsis="false" v-model="selectTab" @change="changeTabs"
+            :swipeable="true">
+            <van-tab v-for="item in dataList" :title="item.title" :name="item.id" :key="item.id">
               <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
                 <ul>
                   <li v-for="item in contentList" class="contentList">
@@ -68,84 +69,101 @@
 
 <script>
   // import 'vant/lib/button/style';
- import request from '@/api/request.js'
+  import request from '@/api/request.js'
   export default {
     name: 'mainNavBar',
-    props:{classId:{default:''}},
+    props: { classId: { default: '' } },
     data() {
       return {
-        selectTab:'',//选中的标签
-        userInfo:{},
+        selectTab: '',//选中的标签
+        userInfo: {},
         list: [],
-        pageSize:5,//页尺寸
-        page:2,//页数
-        psize:10,
+        pageSize: 5,//页尺寸
+        page: 2,//页数
+        psize: 10,
         loading: false,
         finished: false,
         active: 1,
         dataList: [{ text: '全部', id: 'all' }, { text: '最新', id: '1' },
         { text: '就业', id: '2' }, { text: '专业', id: '3' }, { text: '辅导', id: '4' }, { text: '辅导班', id: '5' }],
         contentList: [
-          {
-            "id": 4,
-            "columns": 1,
-            "column_name": "",
-            "name": "姓名",
-            "avatar": "https:\/\/www.i2f2f.com\/attachment\/images\/26\/2019\/04\/giXIQxrG74ZXPnLnnFxnd4Rn0QpCFP.jpg",
-            "title": "标题",
-            "image": "https:\/\/www.i2f2f.com\/attachment\/images\/26\/2019\/04\/giXIQxrG74ZXPnLnnFxnd4Rn0QpCFP.jpg",
-            "content": "",
-            "create_time": "2019-11-29 00:00:00"
-          },
-          {
-            "id": 5,
-            "columns": 2,
-            "column_name": "",
-            "name": "12321312",
-            "avatar": "https:\/\/www.i2f2f.com\/attachment\/images\/26\/2019\/04\/giXIQxrG74ZXPnLnnFxnd4Rn0QpCFP.jpg",
-            "title": "12321312",
-            "image": "https:\/\/www.i2f2f.com\/attachment\/images\/26\/2019\/04\/giXIQxrG74ZXPnLnnFxnd4Rn0QpCFP.jpg",
-            "content": "",
-            "create_time": "2019-11-29 00:00:00"
-          }
+          // {
+          //   "id": 4,
+          //   "columns": 1,
+          //   "column_name": "",
+          //   "name": "姓名",
+          //   "avatar": "https:\/\/www.i2f2f.com\/attachment\/images\/26\/2019\/04\/giXIQxrG74ZXPnLnnFxnd4Rn0QpCFP.jpg",
+          //   "title": "标题",
+          //   "image": "https:\/\/www.i2f2f.com\/attachment\/images\/26\/2019\/04\/giXIQxrG74ZXPnLnnFxnd4Rn0QpCFP.jpg",
+          //   "content": "",
+          //   "create_time": "2019-11-29 00:00:00"
+          // },
+          // {
+          //   "id": 5,
+          //   "columns": 2,
+          //   "column_name": "",
+          //   "name": "12321312",
+          //   "avatar": "https:\/\/www.i2f2f.com\/attachment\/images\/26\/2019\/04\/giXIQxrG74ZXPnLnnFxnd4Rn0QpCFP.jpg",
+          //   "title": "12321312",
+          //   "image": "https:\/\/www.i2f2f.com\/attachment\/images\/26\/2019\/04\/giXIQxrG74ZXPnLnnFxnd4Rn0QpCFP.jpg",
+          //   "content": "",
+          //   "create_time": "2019-11-29 00:00:00"
+          // }
         ],
         activeIndex: '1',
         activeIndex2: '1'
       }
     },
     methods: {
-      changeTabs(name,title){
-        var _this=this;
-        console.log(name,title);
-        var data={
-          page:1,
-          psize:this.psize,
-          class:1,
-          column:this.selectTab
+      changeTabs(name, title) {
+        var _this = this;
+        console.log(name, title);
+        var data = {
+          page: 1,
+          psize: this.psize,
+          class: 1,
+          column: this.selectTab
         }
-        request.post('/roomapi/Room_Class/classPage',data,function(res){
-          _this.contentList=res.data.model;
+        request.post('/roomapi/Room_Class/classPage', data, function (res) {
+          _this.contentList = res.data.model;
         })
       },
       handleSelect(key, keyPath) {
         console.log(key, keyPath);
       },
-      getUserInfo(){
-        var userInfo={}
-        this.userInfo=JSON.parse(sessionStorage.getItem('userInfo')); 
-        console.log('user***',userInfo)
+      getUserInfo() {
+        var userInfo = {}
+        this.userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
+        console.log('user***', userInfo)
         return userInfo;
       },
-      getColmn(id){//获取栏目
-      this.getUserInfo();
-      let userInfoId=this.userInfo.class_id;
-      var _this=this;
-      var data={cid:userInfoId,type:1}
-        request.post('/roomapi/Room_Class/column',data,function(res){
-          _this.dataList=res.data
+      getColmn(id) {//获取栏目
+        this.getUserInfo();
+        let userInfoId = this.userInfo.class_id;
+        var _this = this;
+        var data = { cid: userInfoId, type: 1 }
+        request.post('/roomapi/Room_Class/column', data, function (res) {
+          _this.dataList = res.data;
+          debugger
+          data = {
+            class: _this.userInfo.class_id,
+            column:res.data[0]?res.data[0].id:0,
+            page: 1
+          }
+          request.post('/roomapi/Room_Class/classPage', data, function (res) {//获取数据
+            _this.contentList = res.data.model;
+            if( _this.contentList.length==0){
+            _this.onLoad("fineshed");
+            }
+          });
         });
       },
-      onLoad() {
+      onLoad(state) {
+        if(state=="fineshed"){
+            _this.loading = false;
+            _this.finished = true;
+            return;
+        }
         // 异步更新数据
         // setTimeout(() => {
         //   for (let i = 0; i < 10; i++) {
@@ -169,22 +187,22 @@
         //     this.finished = true;
         //   }
         // }, 500);
-        var _this=this;
-     var   data={
-          page:this.page,
-          psize:this.psize,
-          class:1,
-          column:this.selectTab
+        var _this = this;
+        var data = {
+          page: this.page,
+          psize: this.psize,
+          class: 1,
+          column: this.selectTab
         }
-        request.post('/roomapi/Room_Class/classPage',data,function(res){
-          if(res.data.model.length==0){
+        request.post('/roomapi/Room_Class/classPage', data, function (res) {
+          if (res.data.model.length == 0) {
             _this.loading = false;
             _this.finished = true;
             return;
           }
-          _this.page=_this.page+1;
-          _this.contentList=[...res.data.model,..._this.contentList];
-          _this.loading = false;          
+          _this.page = _this.page + 1;
+          _this.contentList = [...res.data.model, ..._this.contentList];
+          _this.loading = false;
         })
       },
       fslip(item) {
@@ -200,16 +218,16 @@
     mounted: function () {
       this.getUserInfo();
 
-      var data={
-        class:this.userInfo.class_id,
-        column:this.selectTab,
-        page:1
+      var data = {
+        class: this.userInfo.class_id,
+        column: this.selectTab,
+        page: 1
       }
-      var _this=this;
+      var _this = this;
       this.getColmn(data.class_id);
-      request.post('/roomapi/Room_Class/classPage',data,function(res){
-          _this.contentList=res.data.model;
-      });
+      // request.post('/roomapi/Room_Class/classPage',data,function(res){
+      //     _this.contentList=res.data.model;
+      // });
     },
     wrap() {
       var clientWidth = document.body.clientWidth;
@@ -230,7 +248,9 @@
   .mainleftbar {
     /* padding-right:40px; */
   }
-
+  .van-tabs__content{
+    min-height: 200px;
+  }
   #mainNavBar .el-menu--horizontal>.el-submenu .el-submenu__title {
     line-height: 44px;
     height: 44px;
